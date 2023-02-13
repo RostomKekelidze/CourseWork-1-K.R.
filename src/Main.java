513b727036f159af2a92c637180c63616a237aa7
Kekelidze Rostom <r.pe4at@yandex.ru>
2023-02-13 18:49:21 +0300

    Курсовая работа начало

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Random;
+
 public class Main {
+    private static final Employee[] EMPLOYEES = new Employee[10];
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        fullEmployees();
+        printFullInfo();
     }
-}
+
+
+
+        private static void fullEmployees(){
+            Random random= new Random();
+            for (int i = 0; i < EMPLOYEES.length; i++) {
+                EMPLOYEES[0] = new Employee
+                        ("Сотрудник "+ (i+ 1),
+                                random.nextInt(6),
+                                random.nextInt(45_000));
+            }
+        }
+
+
+
+
+
+    private static void printFullInfo() {
+                for (Employee employee : EMPLOYEES) {
+                    System.out.println(employee);
+
+                }
+            }
+        }
+
